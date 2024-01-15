e13420baf8145e53f159962b1a3ac5f27fcea382
JohnReaJR <johnrea.njr1@gmail.com>
2024-01-15 11:43:41 +0300

    Update install6.sh

--- install6.sh
+++ install6.sh
@@ -63,6 +63,11 @@ case $selected_option in
 EOF
         # [+config+]
         chmod +x /root/udp/config.json
+        
+make_service() {
+  ip_nat=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | cut -d '/' -f 1 | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | sed -n 1p)
+  interface=$(ip -4 addr | grep inet | grep -vE '127(\.[0-9]{1,3}){3}' | grep "$ip_nat" | awk {'print $NF'})
+  public_ip=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<<"$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 
         cat <<EOF >/etc/systemd/system/request-server.service
 [Unit]
