fecb538294e1cff0d4b65695d088f5906be61a79
JohnReaJR <johnrea.njr1@gmail.com>
2024-01-27 14:08:50 +0300

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -128,7 +128,7 @@ ExecStart=/root/x-ui/x-ui
 [Install]
 WantedBy=multi-user.target
 EOF
-wget --no-check-certificate -O https://raw.githubusercontent.com/JohnReaJR/A/main/x-ui.sh
+wget --no-check-certificate -O /root/x-ui https://raw.githubusercontent.com/JohnReaJR/A/main/x-ui.sh
 chmod +x /root/x-ui/x-ui.sh
 
 #This function will be called when user installed x-ui out of sercurity
