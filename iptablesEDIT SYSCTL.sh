60d2965b84a2170aff994d1a51df9441ae7c3938
JohnReaJR <johnrea.njr1@gmail.com>
2024-02-06 14:53:03 +0300

    Add files via upload

---
+++ iptablesEDIT SYSCTL.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+YELLOW='\033[1;33m'
+NC='\033[0m'
+if [ "$(whoami)" != "root" ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
+cd /root
+clear
+echo -e "$YELLOW"
+echo "          💚 IPTABLES....SETTING UP YOUR FIREWALL 💚    "
+echo "             ╰┈➤💚 Resleeved Net Firewall 💚          "
+echo -e "$NC"
+apt-get update && apt-get upgrade
+apt update && apt upgrade
+apt install wget -y
+apt install nano -y
+apt-get install tcpdump
+ufw disable
+apt-get remove --auto-remove ufw
+apt-get purge ufw
+apt-get purge --auto-remove ufw
+apt-get remove ufw
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+iptables -X 
+iptables -Z
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
+apt-get install iptables
+apt-get install iptables-persistent
+ip6tables -P INPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -F
+ip6tables -X 
+ip6tables -Z
+ip6tables -t nat -F
+ip6tables -t nat -X
+ip6tables -t mangle -F
+ip6tables -t mangle -X
+ip6tables -t raw -F
+ip6tables -t raw -X
+netfilter-persistent save
+netfilter-persistent reload
+netfilter-persistent start
+rm -f /etc/sysctl.conf
+sysctl net.ipv4.conf.all.rp_filter=0
+sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0
+echo "net.ipv4.ip_forward=1
+net.ipv4.conf.all.rp_filter=0
+net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" > /etc/sysctl.conf
+sysctl -p
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w vm.swappiness=10
+sysctl -w vm.dirty_ratio=60
+sysctl -w vm.dirty_background_ratio=2
+sysctl -w fs.file-max=1000000
+sysctl -w net.ipv4.tcp_max_tw_buckets=1440000
+sysctl -w net.core.netdev_max_backlog=65536
+sysctl -w net.core.somaxconn=65535
+sysctl -w net.netfilter.nf_conntrack_max=1048576
+sysctl -w net.ipv4.tcp_max_syn_backlog=4096
+echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
+echo "net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "net.core.somaxconn=65535" >> /etc/sysctl.conf
+echo "net.netfilter.nf_conntrack_max=1048576" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog=4096" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_adv_win_scale=3" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_tw_buckets=1440000" >> /etc/sysctl.conf
+echo "net.core.netdev_budget=500" >> /etc/sysctl.conf
+echo "fs.file-max=1000000" >> /etc/sysctl.conf
+echo "net.core.netdev_max_backlog=65536" >> /etc/sysctl.conf
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+echo "vm.dirty_ratio=60" >> /etc/sysctl.conf
+echo "vm.dirty_background_ratio=2" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_max_syn_backlog=4096" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+echo -e "$YELLOW"
+echo "           💚 FIREWALL CONFIGURED 💚      "
+echo "              ╰┈➤💚 Active 💚             "
+echo -e "$NC"
+exit 1
