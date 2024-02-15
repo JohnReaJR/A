b428015eb6cb9ed3f688d031022776eb6541c181
JohnReaJR <johnrea.njr1@gmail.com>
2024-02-15 22:36:09 +0300

    Update install10.sh

--- install10.sh
+++ install10.sh
@@ -82,7 +82,7 @@ time_reboot() {
         rm -f /root/udp/config.json
         cat <<EOF >/root/udp/config.json
 {
-  "listen": ":443",
+  "listen": ":444",
   "stream_buffer": 16777216,
   "receive_buffer": 33554432,
   "auth": {
