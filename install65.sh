1e4b4f2af8bfc8037935fb740476cfff6146135e
JohnReaJR <johnrea.njr1@gmail.com>
2024-02-22 05:30:26 +0300

    Update install65.sh

--- install65.sh
+++ install65.sh
@@ -54,6 +54,7 @@ case $selected_option in
         sudo usermod -a -G mita root
         cat <<EOF >/root/Mita_Config_Server.json
         { "portBindings" : [ { "portRange" : "20000-50000", "protocol" : "TCP" }, { "port" : 10000 , "protocol" : "TCP" } ], "users" : [ { "name" : "Resleeved" , "password" : "Resleeved" } ], "loggingLevel" : "INFO" , "mtu" : 1400 }
+        EOF
         # [+config+]
         chmod 755 /root/Mita_Config_Server.json
         #Start Services
