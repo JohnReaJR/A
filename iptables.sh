fc00fcea7c1bb349ec7960ee3cc3523df8b26b85
JohnReaJR <johnrea.njr1@gmail.com>
2024-02-22 05:03:51 +0300

    Create iptables.sh

---
+++ iptables.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+YELLOW='\033[1;33m'
+NC='\033[0m'
+if [ "$(whoami)" != "root" ]; then
+    echo "Error: This script must be run as root."
+    exit 1
+fi
+cd /root
+clear
+echo -e "$YELLOW"
+echo "          💚 IPTABLES....SETTING UP YOUR FIREWALL 💚    "
+echo "             ╰┈➤💚 Resleeved Net Firewall 💚          "
+echo -e "$NC"
+apt-get update && apt-get upgrade
+apt update && apt upgrade
+apt install wget
+apt install net-tools
+ufw disable
+apt-get remove --purge ufw firewalld -y
+apt-get remove ufw
+apt-get remove --auto-remove ufw
+apt-get purge ufw
+apt-get purge --auto-remove ufw
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -F
+iptables -X 
+iptables -Z
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
+apt-get install iptables
+apt-get install iptables-persistent
+ip6tables -P INPUT ACCEPT
+ip6tables -P FORWARD ACCEPT
+ip6tables -P OUTPUT ACCEPT
+ip6tables -F
+ip6tables -X 
+ip6tables -Z
+ip6tables -t nat -F
+ip6tables -t nat -X
+ip6tables -t mangle -F
+ip6tables -t mangle -X
+ip6tables -t raw -F
+ip6tables -t raw -X
+iptables -t nat -A POSTROUTING -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -j MASQUERADE
+netfilter-persistent save
+netfilter-persistent reload
+netfilter-persistent start
+rm -f /etc/sysctl.conf
+sysctl net.ipv4.conf.all.rp_filter=0
+sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0
+echo "net.ipv4.ip_forward=1
+net.ipv4.conf.all.rp_filter=0
+net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" >> /etc/sysctl.conf
+sysctl -p
+sysctl -w net.core.rmem_default=83886080
+sysctl -w net.core.wmem_default=83886080
+sysctl -w net.core.rmem_max=16777216
+sysctl -w net.core.wmem_max=16777216
+sysctl -w net.ipv4.tcp_rmem=8192
+sysctl -w net.ipv4.tcp_wmem=8192
+sysctl -w vm.swappiness=10
+sysctl -w vm.dirty_ratio=60
+sysctl -w vm.dirty_background_ratio=2
+echo "net.core.rmem_default=83886080" >> /etc/sysctl.conf
+echo "net.core.wmem_default=83886080" >> /etc/sysctl.conf
+echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
+echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
+echo "net.core.default_qdisc=fq
+net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "vm.swappiness=10" >> /etc/sysctl.conf
+echo "vm.dirty_ratio=60" >> /etc/sysctl.conf
+echo "vm.dirty_background_ratio=2" >> /etc/sysctl.conf
+sysctl -p /etc/sysctl.conf
+echo -e "$YELLOW"
+echo "           💚 FIREWALL CONFIGURED 💚      "
+echo "              ╰┈➤💚 Active 💚             "
+echo -e "$NC"
+exit 1
