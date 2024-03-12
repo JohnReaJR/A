7c1345b5223e6124e3b547e31616e7852535f5b2
JohnReaJR <johnrea.njr1@gmail.com>
2024-03-13 00:43:53 +0300

    Update tcp.sh

--- tcp.sh
+++ tcp.sh
@@ -9,7 +9,7 @@ cd /root
 clear
 echo -e "$YELLOW"
 echo "          💚 DNS2TCP INSTALLATION SCRIPT 💚    "
-echo "        ╰┈➤💚 Installing DNSTT Binaries 💚          "
+echo "        ╰┈➤💚 Installing DNS2TCP Binaries 💚          "
 echo -e "$NC"
 cd /root
 rm -rf dns2tcp
@@ -20,20 +20,13 @@ apt-get remove --auto-remove dns2tcp
 apt-get purge dns2tcp
 apt-get purge --auto-remove dns2tcp
 apt-get install dns2tcp
-echo -e "$YELLOW"
-read -p "In this step, you will uncomment DNS and write DNS=1.1.1.1 and uncomment DNSStubListener and write DNSStubListener=no"
-echo -e "$NC"
-nano /etc/systemd/resolved.conf
-echo -e "$YELLOW"
-read -p "by tapping 'Enter', you make sure that you have uncomment DNS=1.1.1.1 and DNSStubListener=no"
-echo -e "$NC"
 if lsof -i :53 | grep -q ":53"; then
     echo -e "$YELLOW"
     echo "Error : there is a program that already uses the port 53"
     echo -e "$NC"
     exit 1
 fi
-systemctl restart systemd-resolved
+cd /root
 mkdir dns2tcp
 cd dns2tcp
 mkdir /var/empty
