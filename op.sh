e5804dea63024cd3123f65f15bd13b1d45af04bd
JohnReaJR <johnrea.njr1@gmail.com>
2024-03-26 19:38:39 +0300

    Update op.sh

--- op.sh
+++ op.sh
@@ -14,14 +14,10 @@ echo 'Installing dependencies.'
 {
 export DEBIAN_FRONTEND=noninteractive
 apt update
-apt install -y curl wget cron python-minimal libpython-stdlib
-apt install -y iptables
-apt install -y openvpn netcat httpie php neofetch vnstat
-apt install -y screen squid stunnel4 dropbear gnutls-bin python
-apt install -y dos2unix nano unzip jq virt-what net-tools default-mysql-client
-apt install -y mlocate dh-make libaudit-dev build-essential fail2ban
+apt install iptables
+apt install openvpn
 clear
-}&>/dev/null
+}
 clear
 }
 install_openvpn()
