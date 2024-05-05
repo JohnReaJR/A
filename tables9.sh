c4503982b44e70a4eb0dc7bdd404e6a6009a4a60
JohnReaJR <johnrea.njr1@gmail.com>
2024-05-05 15:19:14 +0300

    Update tables9.sh

--- tables9.sh
+++ tables9.sh
@@ -11,8 +11,8 @@ echo -e "$YELLOW"
 echo "          💚 IPTABLES....SETTING UP YOUR FIREWALL 💚    "
 echo "             ╰┈➤💚 Resleeved Net Firewall 💚          "
 echo -e "$NC"
-apt-get update
-apt update
+apt-get update && apt-get upgrade
+apt update && apt upgrade
 apt install wget
 apt install net-tools
 ufw disable
@@ -24,35 +24,6 @@ apt-get remove
 apt-get autoremove
 apt-get clean
 apt-get autoclean
-iptables -P INPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -F
-iptables -X 
-iptables -Z
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
-iptables -t raw -F
-iptables -t raw -X
-apt-get install iptables
-apt-get install iptables-persistent
-ip6tables -P INPUT ACCEPT
-ip6tables -P FORWARD ACCEPT
-ip6tables -P OUTPUT ACCEPT
-ip6tables -F
-ip6tables -X 
-ip6tables -Z
-ip6tables -t nat -F
-ip6tables -t nat -X
-ip6tables -t mangle -F
-ip6tables -t mangle -X
-ip6tables -t raw -F
-ip6tables -t raw -X
-netfilter-persistent save
-netfilter-persistent reload
-netfilter-persistent start
 cd /root
 rm -rf /etc/sysctl.conf
 sysctl net.ipv4.conf.all.rp_filter=0
