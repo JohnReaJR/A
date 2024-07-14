d1ef696bd716214dcd51b3ebdcd6d6c66984c6fd
JohnReaJR <johnrea.njr1@gmail.com>
2024-07-14 13:12:51 +0300

    Update install10.sh

--- install10.sh
+++ install10.sh
@@ -82,7 +82,7 @@ time_reboot() {
         rm -rf /root/udp/config.json
         cat <<EOF >/root/udp/config.json
 {
-  "listen": ":36718",
+  "listen": "$(curl -s https://api.ipify.org):36718",
   "stream_buffer": 16777216,
   "receive_buffer": 83886080,
   "auth": {
